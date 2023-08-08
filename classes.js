73268e3d11c79a18797925316ff602dd4507851f
PaulaTam <ptam4@sfsu.edu>
2023-08-08 11:19:05 -0700

    created Ghost class

--- classes.js
+++ classes.js
@@ -18,7 +18,7 @@ export class Boundary {
 }
 
 export class Player { //Pacman
-    constructor({position, velocity}) {
+    constructor({ position, velocity }) {
         this.position = position;
         this.velocity = velocity;
         this.radius = 15;
@@ -37,5 +37,64 @@ export class Player { //Pacman
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
+    }
+}
+
+export class Ghost { //Pacman
+    static speed = 2;
+    constructor({ position, velocity, color}) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 15;
+        this.color = color;
+        this.speed = 2;
+        this.prevCollision = [];
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y,
+            this.radius, 0, Math.PI * 2);
+        c.fillStyle = this.color;
+        c.fill();
+        c.closePath();
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
+
+export class Pellet { //pellets
+    constructor({ position }) {
+        this.position = position;
+        this.radius = 3;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y,
+            this.radius, 0, Math.PI * 2);
+        c.fillStyle = 'white';
+        c.fill();
+        c.closePath();
+    }
+}
+
+export class SuperPellet {
+    constructor({ position }) {
+        this.position = position;
+        this.radius = 6;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y,
+            this.radius, 0, Math.PI * 2);
+        c.fillStyle = 'white';
+        c.fill();
+        c.closePath();
     }
 }
