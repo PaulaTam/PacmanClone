96fce3c3dbdf343b0bd2ac20218a3e935cb671bf
PaulaTam <ptam4@sfsu.edu>
2023-08-08 11:20:22 -0700

    created Ghosts + score display

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import { Boundary, Player } from './classes.js';
+import { Boundary, Player, Pellet, SuperPellet, Ghost } from './classes.js';
 import { animate } from './functions.js';
 
 export const canvas = document.getElementById("myCanvas"); //look for canvas tag
 export const c = canvas.getContext('2d'); // canvas context, using 2d
+
+export const curScore = document.getElementById("curScore"); //get score
 
 canvas.width = innerWidth; //window.innerWidth
 canvas.height = innerHeight; //window.innerHeight
@@ -11,23 +13,70 @@ console.log(canvas);
 
 //draw map
 const map = [
-    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-    ['-', ' ', ' ', ' ', '-', '-', ' ', ' ', ' ',  '-'],
-    ['-', ' ', '-', ' ', ' ', ' ', ' ', '-', ' ',  '-'],
-    ['-', ' ', ' ', ' ', '-', '-', ' ', ' ', ' ',  '-'],
-    ['-', ' ', '-', ' ', ' ', ' ', ' ', '-', ' ',  '-'],
-    ['-', ' ', ' ', ' ', '-', '-', ' ', ' ', ' ',  '-'],
-    ['-', '-', ' ', '-', ' ', ' ', '-', ' ', '-',  '-'],
-    ['-', '-', ' ', '-', ' ', ' ', '-', ' ', '-',  '-'],
-    ['-', ' ', ' ', ' ', '-', '-', ' ', ' ', ' ',  '-'],
-    ['-', ' ', '-', ' ', ' ', ' ', ' ', '-', ' ',  '-'],
-    ['-', ' ', ' ', ' ', '-', '-', ' ', ' ', ' ',  '-'],
-    ['-', ' ', '-', ' ', ' ', ' ', ' ', '-', ' ',  '-'],
-    ['-', ' ', ' ', ' ', '-', '-', ' ', ' ', ' ',  '-'],
-    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '*', '.', '.', '-', '-', '-', '.', '.', '*', '-'],
+    ['-', '.', '-', '.', '.', '.', '.', '.', '-', '.', '-'],
+    ['-', '.', '.', '.', '-', '-', '-', '.', '.', '.', '-'],
+    ['-', '.', '-', '.', '.', '.', '.', '.', '-', '.', '-'],
+    ['-', '.', '.', '.', '-', ' ', '-', '.', '.', '.', '-'],
+    ['-', '-', '.', '-', ' ', ' ', ' ', '-', '.', '-', '-'],
+    ['-', '-', '.', '-', ' ', '-', ' ', '-', '.', '-', '-'],
+    ['-', '-', '.', '-', ' ', ' ', ' ', '-', '.', '-', '-'],
+    ['-', '.', '.', '.', '-', '-', '-', '.', '.', '.', '-'],
+    ['-', '.', '-', '.', '.', '.', '.', '.', '-', '.', '-'],
+    ['-', '.', '.', '.', '-', '-', '-', '.', '.', '.', '-'],
+    ['-', '.', '-', '.', '.', '.', '.', '.', '-', '.', '-'],
+    ['-', '*', '.', '.', '-', '-', '-', '.', '.', '*', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
 ];
 
 export const boundaries = [];
+export const pellets = [];
+export const ghosts = [
+    new Ghost( {
+        position: {
+            x: Boundary.width/2 + Boundary.width * 4,
+            y: Boundary.height/2 + Boundary.height * 6
+        },
+        velocity: {
+            x: Ghost.speed,
+            y: 0
+        },
+        color: "red"   
+    }),
+    new Ghost( {
+        position: {
+            x: Boundary.width/2 + Boundary.width * 4,
+            y: Boundary.height/2 + Boundary.height * 8
+        },
+        velocity: {
+            x: Ghost.speed,
+            y: 0
+        },
+        color: "orange"   
+    }),
+    new Ghost( {
+        position: {
+            x: Boundary.width/2 + Boundary.width * 6,
+            y: Boundary.height/2 + Boundary.height * 8
+        },
+        velocity: {
+            x: 0,
+            y: -Ghost.speed
+        },
+        color: "lightblue"   
+    }),
+    new Ghost( {
+        position: {
+            x: Boundary.width/2 + Boundary.width * 6,
+            y: Boundary.height/2 + Boundary.height * 6
+        },
+        velocity: {
+            x: 0,
+            y: Ghost.speed
+        },
+        color: "pink"   
+    }) ];
 
 map.forEach((row, i) => {
     row.forEach((symbol, j) => {
@@ -41,6 +90,26 @@ map.forEach((row, i) => {
                         }
                     })
                 )
+                break;
+            case '.':
+                pellets.push(
+                    new Pellet({
+                        position: {
+                            x: Boundary.width/2 + Boundary.width * j,
+                            y: Boundary.height/2 + Boundary.height * i
+                        }
+                    })
+                )
+                break;
+            case '*':
+                pellets.push(
+                    new SuperPellet({
+                        position: {
+                            x: Boundary.width/2 + Boundary.width * j,
+                            y: Boundary.height/2 + Boundary.height * i
+                        }
+                    })
+                )
             break;
         };
     });
@@ -49,8 +118,8 @@ map.forEach((row, i) => {
 //draw player pacman
 export const player = new Player({
     position: {
-        x: Boundary.width + Boundary.width / 2,
-        y: Boundary.height + Boundary.height / 2
+        x: Boundary.width/2 + Boundary.width * 5,
+        y: Boundary.height/2 + Boundary.height * 10
     },
 
     velocity: {
